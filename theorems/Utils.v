5743d382ba3a9ddbff1968cc02aa3054c5a65d60
Blaine Hansen <faichenshing@gmail.com>
2021-08-06 19:57:50 -0600

    first simple version of small step relation

--- theorems/Utils.v
+++
@@ -1,6 +0,0 @@
-Inductive Result (T: Type) (E: Type): Type :=
-	| Ok (value: T)
-	| Err (error: E).
-
-Arguments Ok {T} {E} _.
-Arguments Err {T} {E} _.
--- theorems/main.v
+++ theorems/main.v
@@ -3,42 +3,135 @@ Import ListNotations.
 
 From stdpp Require Import base fin vector options.
 
-Definition Register := nat.
-Definition RegisterBank n := vec Register n.
-
 Example t1: ([# 4; 2] !!! 0%fin) = 4.
 Proof. reflexivity. Qed.
 
-Definition label := option string.
+
+Section Sized.
+	Variable size: nat.
+	Notation RegisterBank := (vec nat size).
+	Notation register := (fin size).
+
+	Inductive Operand: Type :=
+		| Literal (n: nat)
+		| Register (r: register)
+	.
+
+	Definition eval_operand
+		(bank: RegisterBank) (operand: Operand)
+	: nat :=
+		match operand with
+		| Literal n => n
+		| Register r => (bank !!! r)
+		end
+	.
+
+	(*Definition label := option string.*)
+	Inductive Instruction :=
+		| InstMov (src: Operand) (dest: register)
+		| InstAdd (val: Operand) (dest: register)
+		(*| InstBranchEq (a: Operand) (b: Operand) (to: label)*)
+		(*| InstBranchNeq (a: Operand) (b: Operand) (to: label)*)
+		| InstExit
+	.
+
+	(*Record State := state { counter: nat; bank: RegisterBank }.*)
+
+	Inductive step
+		{program: list Instruction} {counter} {bank}
+	: nat -> RegisterBank -> Prop :=
+		| step_Mov: forall src dest,
+			(lookup counter program) = Some (InstMov src dest)
+			-> step
+				(S counter)
+				(vinsert dest (eval_operand bank src) bank)
+		| step_Add: forall val dest,
+			(lookup counter program) = Some (InstAdd val dest)
+			-> step
+				(S counter)
+				(vinsert dest ((eval_operand bank val) + (bank !!! dest)) bank)
+	.
+
+	Fixpoint execute_program_unsafe
+		(fuel: nat) (program: list Instruction)
+		(counter: nat) (bank: RegisterBank)
+	: option RegisterBank :=
+		match (lookup counter program) with
+		| None => None
+		| Some inst => match fuel, inst with
+			| _, InstExit => Some bank
+			| 0, _ => None
+			| S f, InstMov src dest => execute_program_unsafe
+				f program (S counter)
+				(vinsert dest (eval_operand bank src) bank)
+			| S f, InstAdd val dest => execute_program_unsafe
+				f program (S counter)
+				(vinsert dest ((eval_operand bank val) + (bank !!! dest)) bank)
+			end
+		end
+	.
+End Sized.
+
+Arguments Literal {size} _.
+Arguments Register {size} _.
+
+Arguments execute_program_unsafe {size} _ _ _ _.
+
+Arguments InstMov {size} _ _.
+Arguments InstAdd {size} _ _.
+(*Arguments InstBranchEq {size} _ _ _.*)
+(*Arguments InstBranchNeq {size} _ _ _.*)
+Arguments InstExit {size}.
 
 
-Inductive Operand :=
-	| Literal (n: nat)
-	| Register (r: register)
-.
+Definition redundant_additions: list (Instruction 1) := [
+	InstMov (Literal 0) (0%fin);
+	InstAdd (Literal 1) (0%fin);
+	InstAdd (Literal 1) (0%fin);
+	InstAdd (Literal 1) (0%fin);
+	InstAdd (Literal 1) (0%fin);
+	InstAdd (Literal 1) (0%fin);
+	InstExit
+].
+Example test__redundant_additions:
+	execute_program_unsafe
+		(length redundant_additions) redundant_additions 0 [#0] = Some [#5].
+Proof. reflexivity. Qed.
 
-Notation RegisterBank := list nat (only parsing).
 
-Inductive Instruction :=
-	| InstMov (l: label) (src: Operand) (dest: register)
-	| InstAdd (l: label) (a: Operand) (dest: register)
-	(*| InstBranchEq (l: label) (a: Operand) (b: Operand) (to: label)*)
-	| InstBranchNeq (l: label) (a: Operand) (b: Operand) (to: label)
-	| InstExit (l: label) (return_val: Operand)
-.
-
+Definition redundant_doubling: list (Instruction 1) := [
+	InstMov (Literal 1) (0%fin);
+	InstAdd (Register 0%fin) (0%fin);
+	InstAdd (Register 0%fin) (0%fin);
+	InstAdd (Register 0%fin) (0%fin);
+	InstExit
+].
+Example test__redundant_doubling:
+	execute_program_unsafe
+		(length redundant_doubling) redundant_doubling 0 [#0] = Some [#8].
+Proof. reflexivity. Qed.
 
 (*
-I want to create a fixpoint
-execute_program_unsafe
-
 that can run a program to completion but can fail and requires fuel
 then prove that a particular linear program can be run with a *safe* variant since it decreases on a well-founded order
 then prove that any purely dag-like program can be run safely
 then figure out how to prove that any labeled program that proof justifies cyclic jumps can be run safely
-
 *)
 
+(*
+Definition mergeSort: list A -> list A.
+refine (Fix lengthOrder wf (fun => list A)
+	(fun
+		(ls: list A)
+		(mergeSort: forall ls': list A, lengthOrder ls' ls -> list A)
+	=>
+			if le_lt_dec 2 (length ls)
+			then let lss := split ls in
+			merge (mergeSort (fst lss)) (mergeSort (snd lss))
+			else ls)
+); subst lss; eauto.
+Defined.
+*)
 
 Notation val := Operand (only parsing).
 Notation expr := Instruction (only parsing).
@@ -72,3 +165,13 @@ done:
 
 
 Inductive Bit: Type := B0 | B1.
+
+
+
+
+Inductive Result (T: Type) (E: Type): Type :=
+	| Ok (value: T)
+	| Err (error: E).
+
+Arguments Ok {T} {E} _.
+Arguments Err {T} {E} _.
