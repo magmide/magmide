7af64fe58e9ca8f91446dfc66417defc72462d05
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2022-05-24 07:29:44 -0700

    ocaml bindings wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,29 @@
-use anyhow::Result;
+use anyhow::Error;
 use inkwell::{builder::Builder, context::Context, types::IntType, values::IntValue};
 use nom::{
     branch::alt, bytes::complete::tag, character::complete, combinator::map,
     sequence::separated_pair, Finish, IResult,
 };
+use ocaml_interop::{impl_conv_ocaml_variant, ocaml_export, OCaml, OCamlInt, OCamlRef, ToOCaml};
 use std::{fs::read, str::from_utf8};
 
-// #[derive(ocmal::IntoValue, ocaml::FromValue)]
 pub enum AST {
     Add(Box<AST>, Box<AST>),
     Number(i32),
 }
 
-// #[ocaml::func]
-pub fn parse(filename: &str) -> Result<AST> {
+impl_conv_ocaml_variant! {
+    AST {
+        AST::Add(a: AST, b: AST),
+        AST::Number(n: OCamlInt),
+    }
+}
+
+pub fn parse(filename: &str) -> Result<AST, Error> {
     parse_bytes(from_utf8(read(filename)?.as_slice())?)
 }
 
-fn parse_bytes<'a>(i: &'a str) -> Result<AST> {
+fn parse_bytes<'a>(i: &'a str) -> Result<AST, Error> {
     Ok(ast(i).map_err(|err| err.to_owned()).finish().map(|x| x.1)?)
 }
 
@@ -35,18 +41,17 @@ fn ast(i: &str) -> IResult<&str, AST> {
     alt((number, add))(i)
 }
 
-// #[ocaml::func]
 pub fn render(ast: &AST, to: &str) {
-	let context = Context::create();
-	let module = context.create_module("lab");
-	let builder = context.create_builder();
+    let context = Context::create();
+    let module = context.create_module("lab");
+    let builder = context.create_builder();
 
-	let i32_type = context.i32_type();
-	let fn_type = i32_type.fn_type(&[], false);
-	let function = module.add_function("main", fn_type, None);
-	let basic_block = context.append_basic_block(function, "doit");
+    let i32_type = context.i32_type();
+    let fn_type = i32_type.fn_type(&[], false);
+    let function = module.add_function("main", fn_type, None);
+    let basic_block = context.append_basic_block(function, "doit");
 
-	builder.position_at_end(basic_block);
+    builder.position_at_end(basic_block);
 
     fn build<'ctx>(env: (&Builder<'ctx>, IntType<'ctx>), ast: &AST) -> IntValue<'ctx> {
         match ast {
@@ -56,5 +61,18 @@ pub fn render(ast: &AST, to: &str) {
     }
 
     builder.build_return(Some(&build((&builder, i32_type), ast)));
-	module.write_bitcode_to_path(&std::path::Path::new(to));
+    module.write_bitcode_to_path(&std::path::Path::new(to));
+}
+
+ocaml_export! {
+    fn ocaml_parse(cr, filename: OCamlRef<String>) -> OCaml<Result<AST, String>> {
+        let filename: String = filename.to_rust(&cr);
+        parse(filename.as_str()).map_err(|err| format!("{:#}", err)).to_ocaml(cr)
+    }
+
+    fn ocaml_render(cr, ast: OCamlRef<AST>, to: OCamlRef<String>) {
+        let to: String = to.to_rust(&cr);
+        render(&ast.to_rust(&cr), to.as_str());
+        OCaml::unit()
+    }
 }
