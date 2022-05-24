36e3c647f7e489c92c65771afc12dbad65dbe555
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2022-05-24 08:00:36 -0700

    polish notation

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,12 @@ use anyhow::Error;
 use inkwell::{builder::Builder, context::Context, types::IntType, values::IntValue};
 use nom::{
     branch::alt, bytes::complete::tag, character::complete, combinator::map,
-    sequence::separated_pair, Finish, IResult,
+    sequence::{preceded, separated_pair}, Finish, IResult,
 };
 use ocaml_interop::{impl_conv_ocaml_variant, ocaml_export, OCaml, OCamlInt, OCamlRef, ToOCaml};
 use std::{fs::read, str::from_utf8};
 
+#[derive(Debug, PartialEq)]
 pub enum AST {
     Add(Box<AST>, Box<AST>),
     Number(i32),
@@ -19,11 +20,11 @@ impl_conv_ocaml_variant! {
     }
 }
 
-pub fn parse(filename: &str) -> Result<AST, Error> {
-    parse_bytes(from_utf8(read(filename)?.as_slice())?)
+pub fn parse_file(filename: &str) -> Result<AST, Error> {
+    parse(from_utf8(read(filename)?.as_slice())?)
 }
 
-fn parse_bytes<'a>(i: &'a str) -> Result<AST, Error> {
+fn parse<'a>(i: &'a str) -> Result<AST, Error> {
     Ok(ast(i).map_err(|err| err.to_owned()).finish().map(|x| x.1)?)
 }
 
@@ -32,9 +33,14 @@ fn number(i: &str) -> IResult<&str, AST> {
 }
 
 fn add(i: &str) -> IResult<&str, AST> {
-    map(separated_pair(ast, tag("+"), ast), |(a, b)| {
+    map(preceded(tag("+ "), separated_pair(ast, tag(" "), ast)), |(a, b)| {
         AST::Add(Box::new(a), Box::new(b))
     })(i)
+
+//     map(separated_pair(ast, tag("+"), ast), |(a, b)| {
+//         AST::Add(Box::new(a), Box::new(b))
+//     })(i)
+
 }
 
 fn ast(i: &str) -> IResult<&str, AST> {
@@ -67,7 +73,7 @@ pub fn render(ast: &AST, to: &str) {
 ocaml_export! {
     fn ocaml_parse(cr, filename: OCamlRef<String>) -> OCaml<Result<AST, String>> {
         let filename: String = filename.to_rust(&cr);
-        parse(filename.as_str()).map_err(|err| format!("{:#}", err)).to_ocaml(cr)
+        parse_file(filename.as_str()).map_err(|err| format!("{:#}", err)).to_ocaml(cr)
     }
 
     fn ocaml_render(cr, ast: OCamlRef<AST>, to: OCamlRef<String>) {
@@ -75,4 +81,28 @@ ocaml_export! {
         render(&ast.to_rust(&cr), to.as_str());
         OCaml::unit()
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    macro_rules! test_parse {
+        ($name:ident, $in:expr, $out:expr) => {
+            #[test]
+            fn $name() {
+                assert_eq!(parse($in).map_err(|err| format!("{:?}", err)), $out);
+            }
+        }
+    }
+
+    fn num(n: i32) -> AST { AST::Number(n) }
+    fn add(a: AST, b: AST) -> AST { AST::Add(Box::new(a), Box::new(b)) }
+
+    test_parse!(four, "4", Ok(num(4)));
+    test_parse!(plus, "+ 2 3", Ok(add(num(2), num(3))));
+    test_parse!(
+        nest_plus,
+        "+ + 1 + 2 3 4",
+        Ok(add(add(num(1), add(num(2), num(3))), num(4)))
+    );
 }
