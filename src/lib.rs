6067b6d5a89c0a199543392a4b9d205689ec9e3f
Blaine Hansen <faichenshing@gmail.com>
2023-06-08 16:19:21 -0600

    basic module use with path resolution

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ struct Module {
 }
 
 type PathIndex = HashMap<String, ModuleItem>;
+type ModuleCtx = HashMap<String, String>;
 
 fn build_program_path_index(program: Program) -> PathIndex {
 	let mut program_path_index = HashMap::new();
@@ -24,21 +25,70 @@ fn build_program_path_index(program: Program) -> PathIndex {
 
 fn update_program_path_index_with_module(
 	program_path_index: &mut PathIndex,
-	parent_prefix: &str,
+	parent_path: &str,
 	module: Module,
+	// module_ctx: &mut HashMap<String, String>,
 ) {
 	let module_name = module.name;
-	let prefix = format!("{parent_prefix}::{module_name}");
+	let path = format!("{parent_path}::{module_name}");
 	for item in module.items {
-		let item_name = item.give_name();
-		// TODO handle existence case
-		program_path_index.insert(format!("{prefix}::{item_name}"), item);
+		if let Some(item_name) = item.give_name() {
+			// TODO handle conflict case
+			program_path_index.insert(format!("{path}::{item_name}"), item);
+
+			// TODO update module ctx as well
+		}
 	}
 
 	for child_module in module.child_modules {
-		update_program_path_index_with_module(program_path_index, &prefix, child_module);
+		update_program_path_index_with_module(program_path_index, &path, child_module);
 	}
 }
+
+fn build_module_ctx(parent_path: &str, module: &Module) -> ModuleCtx {
+	let mut module_ctx = HashMap::new();
+	let module_name = &module.name;
+	let module_path = format!("{parent_path}::{module_name}");
+	for item in &module.items {
+		match item {
+			ModuleItem::Use(use_tree) => {
+				// TODO format the path onto use_tree.base_path here, including finding stem
+				add_use_tree_to_module_ctx("", &use_tree, &mut module_ctx);
+			},
+			// both of these we just add the resolved path
+			ModuleItem::Prop(PropDefinition { name, .. }) | ModuleItem::Theorem(TheoremDefinition { name, .. }) => {
+				module_ctx.insert(name.into(), format!("{module_path}::{name}"));
+			},
+		}
+	}
+	module_ctx
+}
+
+fn add_use_tree_to_module_ctx(base_path_prefix: &str, use_tree: &UseTree, module_ctx: &mut ModuleCtx) {
+	let base_path = if use_tree.base_path == "self" { base_path_prefix.to_owned() } else {
+		let next_base_path = use_tree.base_path.trim_start_matches("::");
+		format!("{base_path_prefix}::{next_base_path}")
+	};
+
+	match &use_tree.cap {
+		None => {
+			let base_path_stem = base_path.split("::").last().unwrap().to_owned();
+			module_ctx.insert(base_path_stem, base_path);
+		},
+		Some(cap) => match cap {
+			UseTreeCap::All => unimplemented!(),
+			UseTreeCap::AsName(as_name) => {
+				module_ctx.insert(as_name.into(), base_path);
+			},
+			UseTreeCap::InnerTrees(inner_trees) => {
+				for inner_tree in inner_trees {
+					add_use_tree_to_module_ctx(&base_path, &inner_tree, module_ctx);
+				}
+			},
+		},
+	}
+}
+
 
 #[derive(Debug, Clone, PartialEq)]
 enum TypeBody {
@@ -48,6 +98,7 @@ enum TypeBody {
 
 #[derive(Debug, Clone, PartialEq)]
 enum ModuleItem {
+	Use(UseTree),
 	Prop(PropDefinition),
 	Theorem(TheoremDefinition),
 	// Model,
@@ -57,11 +108,45 @@ enum ModuleItem {
 }
 
 impl ModuleItem {
-	fn give_name(&self) -> &String {
+	fn give_name(&self) -> Option<&String> {
 		match self {
-			ModuleItem::Prop(PropDefinition { name, .. }) => name,
-			ModuleItem::Theorem(TheoremDefinition { name, .. }) => name,
+			ModuleItem::Use(_) => None,
+			ModuleItem::Prop(PropDefinition { name, .. }) => Some(name),
+			ModuleItem::Theorem(TheoremDefinition { name, .. }) => Some(name),
 		}
+	}
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
+struct UseTree {
+	base_path: String,
+	cap: Option<UseTreeCap>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum UseTreeCap {
+	All,
+	AsName(String),
+	InnerTrees(Vec<UseTree>),
+}
+
+impl UseTree {
+	fn basic(base_path: &'static str) -> UseTree {
+		UseTree { base_path: base_path.into(), cap: None }
+	}
+	fn basic_as(base_path: &'static str, as_name: &'static str) -> UseTree {
+		UseTree { base_path: base_path.into(), cap: Some(UseTreeCap::AsName(as_name.into())) }
+	}
+}
+
+impl UseTreeCap {
+	fn inners<const N: usize>(static_inner_paths: [&'static str; N]) -> UseTreeCap {
+		let mut inner_paths = vec![];
+		for static_inner_path in static_inner_paths {
+			inner_paths.push(UseTree::basic(static_inner_path));
+		}
+		UseTreeCap::InnerTrees(inner_paths)
 	}
 }
 
@@ -86,6 +171,7 @@ enum Statement {
 		name: String,
 		term: Term,
 	},
+	// InnerModuleItem(ModuleItem),
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -95,20 +181,6 @@ enum Term {
 	// Call
 	// Chain
 }
-
-
-// type ProgramIndex = HashMap<String, ModuleItem>;
-
-// fn index_program(program: Vec<ModuleItem>) -> Option<ProgramIndex> {
-// 	let mut indexed = HashMap::new();
-// 	for program_item in program.into_iter() {
-// 		let key = program_item.give_name().to_owned();
-// 		if indexed.contains_key(&key) { return None }
-
-// 		indexed.insert(key, program_item);
-// 	};
-// 	Some(indexed)
-// }
 
 
 // fn type_check_program(program: Vec<ModuleItem>) -> Option<()> {
@@ -152,16 +224,16 @@ mod tests {
 
 	fn make_trivial_prop() -> ModuleItem {
 		ModuleItem::Prop(PropDefinition {
-			name: "trivial".to_string(),
+			name: "trivial".into(),
 			type_body: TypeBody::Unit,
 		})
 	}
 	fn make_give_trivial_thm() -> ModuleItem {
 		ModuleItem::Theorem(TheoremDefinition {
-			name: "give_trivial".to_string(),
-			return_type: Term::Ident("trivial".to_string()),
+			name: "give_trivial".into(),
+			return_type: Term::Ident("trivial".into()),
 			statements: vec![
-				Statement::Return(Term::Ident("trivial".to_string())),
+				Statement::Return(Term::Ident("trivial".into())),
 			],
 		})
 	}
@@ -172,37 +244,100 @@ mod tests {
 		let give_trivial_thm = make_give_trivial_thm();
 
 		let program_path_index = build_program_path_index(Program { modules: vec![
-			Module { name: "main".to_string(), items: vec![trivial_prop.clone(), give_trivial_thm.clone()], child_modules: vec![] },
+			Module { name: "main".into(), items: vec![trivial_prop.clone(), give_trivial_thm.clone()], child_modules: vec![] },
 		] });
 
 		let expected = HashMap::from([
-			("::main::trivial".to_string(), trivial_prop.clone()),
-			("::main::give_trivial".to_string(), give_trivial_thm.clone()),
+			("::main::trivial".into(), trivial_prop.clone()),
+			("::main::give_trivial".into(), give_trivial_thm.clone()),
 		]);
 		assert_eq!(program_path_index, expected);
 
 
 		let program_path_index = build_program_path_index(Program { modules: vec![
-			Module { name: "main".to_string(), items: vec![trivial_prop.clone(), give_trivial_thm.clone()], child_modules: vec![
-				Module { name: "main_child".to_string(), items: vec![give_trivial_thm.clone()], child_modules: vec![] },
+			Module { name: "main".into(), items: vec![trivial_prop.clone(), give_trivial_thm.clone()], child_modules: vec![
+				Module { name: "main_child".into(), items: vec![give_trivial_thm.clone()], child_modules: vec![] },
 			] },
 
-			Module { name: "side".to_string(), items: vec![trivial_prop.clone(), give_trivial_thm.clone()], child_modules: vec![
-				Module { name: "side_child".to_string(), items: vec![give_trivial_thm.clone()], child_modules: vec![] },
+			Module { name: "side".into(), items: vec![trivial_prop.clone(), give_trivial_thm.clone()], child_modules: vec![
+				Module { name: "side_child".into(), items: vec![give_trivial_thm.clone()], child_modules: vec![] },
 			] },
 		] });
 
 		let expected = HashMap::from([
-			("::main::trivial".to_string(), trivial_prop.clone()),
-			("::main::give_trivial".to_string(), give_trivial_thm.clone()),
-			("::main::main_child::give_trivial".to_string(), give_trivial_thm.clone()),
+			("::main::trivial".into(), trivial_prop.clone()),
+			("::main::give_trivial".into(), give_trivial_thm.clone()),
+			("::main::main_child::give_trivial".into(), give_trivial_thm.clone()),
 
-			("::side::trivial".to_string(), trivial_prop.clone()),
-			("::side::give_trivial".to_string(), give_trivial_thm.clone()),
-			("::side::side_child::give_trivial".to_string(), give_trivial_thm.clone()),
+			("::side::trivial".into(), trivial_prop.clone()),
+			("::side::give_trivial".into(), give_trivial_thm.clone()),
+			("::side::side_child::give_trivial".into(), give_trivial_thm.clone()),
 		]);
 		assert_eq!(program_path_index, expected);
 	}
+
+	#[test]
+	fn test_build_module_ctx() {
+		let module_path = "";
+
+		let side_use = ModuleItem::Use(UseTree {
+			// TODO "bare" references like this are assumed to be "relative", so at the same level as the current module
+			// TODO you could also do super and root
+			base_path: "side".into(),
+			cap: Some(UseTreeCap::inners(["whatever", "other"])),
+		});
+		let module = Module { name: "main".into(), items: vec![side_use, make_trivial_prop(), make_give_trivial_thm()], child_modules: vec![] };
+
+		let expected = HashMap::from([
+			("trivial".into(), "::main::trivial".into()),
+			("give_trivial".into(), "::main::give_trivial".into()),
+			("whatever".into(), "::side::whatever".into()),
+			("other".into(), "::side::other".into()),
+		]);
+		assert_eq!(build_module_ctx(module_path, &module), expected);
+
+
+		let side_use = ModuleItem::Use(UseTree {
+			base_path: "::side::child".into(),
+			cap: Some(UseTreeCap::InnerTrees(vec![
+				UseTree::basic("whatever"),
+				UseTree::basic_as("other", "different"),
+				UseTree { base_path: "nested::thing".into(), cap: Some(UseTreeCap::inners(["self", "a", "b"])) },
+			])),
+		});
+		let module = Module { name: "main".into(), items: vec![side_use, make_trivial_prop(), make_give_trivial_thm()], child_modules: vec![] };
+
+		let expected = HashMap::from([
+			("trivial".into(), "::main::trivial".into()),
+			("give_trivial".into(), "::main::give_trivial".into()),
+			("whatever".into(), "::side::child::whatever".into()),
+			("different".into(), "::side::child::other".into()),
+			("thing".into(), "::side::child::nested::thing".into()),
+			("a".into(), "::side::child::nested::thing::a".into()),
+			("b".into(), "::side::child::nested::thing::b".into()),
+		]);
+		assert_eq!(build_module_ctx(module_path, &module), expected);
+	}
+
+	// #[test]
+	// fn test_resolve_reference() {
+	// 	let program_path_index = HashMap::from([
+	// 		("::main::trivial".into(), make_trivial_prop()),
+	// 		("::main::give_trivial".into(), make_give_trivial_thm()),
+	// 	]);
+	// 	let ctx = HashMap::from([]);
+	// 	let current_path = "::main::";
+
+	// 	assert_eq!(
+	// 		resolve_reference(ctx, current_path, "trivial"),
+	// 		"::main::trivial"
+	// 	);
+
+	// 	let ctx = HashMap::from([
+	// 		("main"),
+	// 	]);
+	// 	let current_path = "::side::";
+	// }
 
 	// #[test]
 	// fn test_resolve_term_type() {
@@ -257,7 +392,7 @@ mod tests {
 	// 	// prop And; (@P, @Q)
 
 	// 	let and_type = TypeDefinition {
-	// 		name: "And".to_string(),
+	// 		name: "And".into(),
 	// 		kind: Kind::Prop,
 	// 		generics: vec![
 	// 			Pattern{name: "P", type: None},
